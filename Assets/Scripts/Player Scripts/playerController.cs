8697f95e2a990136fdbb8be6a636873bcecdc482
ewanw58385 <72386462+ewanw58385@users.noreply.github.com>
2021-05-06 15:55:10 +0100

    Added CharacterController

---
+++ Assets/Scripts/Player Scripts/playerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+    Vector2 movement; 
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        anim.SetFloat("Horizontal", movement.x);
+        anim.SetFloat("Vertical", movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+        if (Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            anim.SetFloat("LastHorizontal", Input.GetAxisRaw("Horizontal"));
+            anim.SetFloat("LastVertical", Input.GetAxisRaw("Vertical"));
+
+        }
+    }  
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+}
